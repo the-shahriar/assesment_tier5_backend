0b64de6d248ee18af361438fba6d9c9532ef63e2
Shahriar Islam <theshahriarislam@gmail.com>
2022-07-17 00:48:36 +0600

    error handler implemented

---
+++ middlewares/error.js
@@ -0,0 +1,15 @@
+module.exports = function (err, req, res, next) {
+  if (err) {
+    let statusCode = 500;
+    if (err.name == "ValidationError") {
+      statusCode = 400;
+    }
+    const errorPaths = Object.keys(err.errors || {});
+    const errorMessages = errorPaths.map(
+      (path) => err.errors[path].properties.message
+    );
+    const errorMessage = errorMessages.join(", ") || err.message;
+
+    res.status(statusCode).json(createResponse(null, errorMessage, true));
+  }
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const device = require("express-device");
 const routes = require("./routes/index");
 const db = require("./db/db");
 const cookieParser = require("cookie-parser");
+const errorHandler = require("./middlewares/error");
 
 require("dotenv").config();
 // Pass the global passport object into the configuration function
@@ -12,13 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use(device.capture());
 app.use(cookieParser());
+app.use(errorHandler);
 app.use("/api", routes);
 
 //error handler
 const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hi to " + req.device.type + " User");
+  res.send("I am running at " + req.device.type);
 });
 
 app.listen(port, () =>
